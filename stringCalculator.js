7e400c8a270537c4e46a43fdb2747aac4238c1d2
shreya <kadshreya07@gmail.com>
2025-04-17 11:19:41 +0530

    Step2:Handle one and two numbers in Add()

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,5 +1,8 @@
 function Add(numbers){
     if(numbers==="") return 0;
+
+    const parts=numbers.split(",").map(Number);
+    return parts.reduce((sum,num)=>sum+num,0)
 }
 
 module.exports={Add}
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -2,3 +2,10 @@ const {Add}=require('./stringCalculator');
 test('returns 0 for an empty string ',()=>{
     expect(Add("")).toBe(0)
 });
+
+test("returns number itself when one number is passed",()=>{
+    expect(Add('5')).toBe(5)
+})
+test("returns sum of two numbers separated by comma",()=>{
+    expect(Add('1,2')).toBe(3)
+})
