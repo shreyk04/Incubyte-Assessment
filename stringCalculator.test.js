b6944701ed592c59558d7d3f7e86eba47b070431
shreya <kadshreya07@gmail.com>
2025-04-17 11:22:35 +0530

    Step3:add multiple comma separted numbers

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -9,3 +9,8 @@ test("returns number itself when one number is passed",()=>{
 test("returns sum of two numbers separated by comma",()=>{
     expect(Add('1,2')).toBe(3)
 })
+
+test("returns sum of multiple comma separted numbers",()=>{
+    expect(Add("2,3,5")).toBe(10)
+    expect(Add("1,2,3,4,5")).toBe(15)
+})
