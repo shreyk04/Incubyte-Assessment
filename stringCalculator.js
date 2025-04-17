78aa87f6a1e15f89ab48a41267207a60c324a480
shreya <kadshreya07@gmail.com>
2025-04-17 23:01:12 +0530

    added counter to track Add method calls

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,36 +1,43 @@
-function Add(numbers) {
-    if (numbers === "") return 0;
+class Calculator{
+     add_method_counter=0
 
-    let delimiter = /,|\n/;
+     getAddMethodCounterValue(){
+        return this.add_method_counter;
+    }
 
-    if (numbers.startsWith("//")) {
-     //extract custom delimiter
-
-        const delimiterLineEnd = numbers.indexOf("\n");
-        const delimiterPart = numbers.slice(2, delimiterLineEnd);
-
-        if (delimiterPart.startsWith("[") && delimiterPart.includes("]")) {
-            const delimiters = delimiterPart.match(/\[(.*?)\]/g).map(d => d.slice(1, -1));
-            delimiter = new RegExp(delimiters.map(d => d.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&')).join('|'));
-        } else {
-            delimiter = delimiterPart;
+     Add(numbers) {
+        this.add_method_counter++;
+        if (numbers === "") return 0;
+    
+        let delimiter = /,|\n/;
+    
+        if (numbers.startsWith("//")) {
+    
+            const delimiterLineEnd = numbers.indexOf("\n");
+            const delimiterPart = numbers.slice(2, delimiterLineEnd);
+    
+            if (delimiterPart.startsWith("[") && delimiterPart.includes("]")) {
+                const delimiters = delimiterPart.match(/\[(.*?)\]/g).map(d => d.slice(1, -1));
+                delimiter = new RegExp(delimiters.map(d => d.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&')).join('|'));
+            } else {
+                delimiter = delimiterPart;
+            }
+    
+            numbers = numbers.slice(delimiterLineEnd + 1);
+    
         }
-
-        numbers = numbers.slice(delimiterLineEnd + 1);
-                //remove cutsom delimiter line
-
+    
+        const parts = numbers.split(delimiter).map(Number);
+    
+    
+        const negatives = parts.filter(num => num < 0);
+        if (negatives.length > 0) {
+            throw new Error("negative numbers are not allowed: " + negatives.join(", "));
+        }
+    
+        return parts.filter(num => num <= 1000).reduce((sum, num) => sum + num, 0);
     }
-
-    const parts = numbers.split(delimiter).map(Number);
-
-        //find all negative numbers
-
-    const negatives = parts.filter(num => num < 0);
-    if (negatives.length > 0) {
-        throw new Error("negative numbers are not allowed: " + negatives.join(", "));
-    }
-
-    return parts.filter(num => num <= 1000).reduce((sum, num) => sum + num, 0);
 }
 
-module.exports = { Add };
+
+module.exports = { Calculator };
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,47 +1,61 @@
-const {Add}=require('./stringCalculator');
+const {Calculator}=require('./stringCalculator');
+
+const calculator=new Calculator();
+
+
+
 test('returns 0 for an empty string ',()=>{
-    expect(Add("")).toBe(0)
+    expect(calculator.Add("")).toBe(0)
 });
 
 test("returns number itself when one number is passed",()=>{
-    expect(Add('5')).toBe(5)
-    expect(Add("9")).toBe(9)
+    expect(calculator.Add('5')).toBe(5)
+    expect(calculator.Add("9")).toBe(9)
 })
 test("returns sum of two numbers separated by comma",()=>{
-    expect(Add('1,2')).toBe(3)
+    expect(calculator.Add('1,2')).toBe(3)
 })
 
 test("returns sum of multiple comma separted numbers",()=>{
-    expect(Add("2,3,5")).toBe(10)
-    expect(Add("1,2,3,4,5")).toBe(15)
+    expect(calculator.Add("2,3,5")).toBe(10)
+    expect(calculator.Add("1,2,3,4,5")).toBe(15)
 })
 
 
 test("supports new lines as delimiters along with commas",()=>{
-    expect(Add("1\n2,3")).toBe(6)
+    expect(calculator.Add("1\n2,3")).toBe(6)
 })
 
 test("supports custom delimiters defined with //delimiter",()=>{
-    expect(Add("//;\n1;2")).toBe(3)
-    // expect(Add("//[\n1\n2")).toBe(3)
+    expect(calculator.Add("//;\n1;2")).toBe(3)
+    // expect(Calculator.Add("//[\n1\n2")).toBe(3)
 })
 
 test("throws an exception when negative numbers are included",()=>{
-    expect(()=>Add("1,-2,3,4,-5")).toThrow("negative numbers are not allowed")
+    expect(()=>calculator.Add("1,-2,3,4,-5")).toThrow("negative numbers are not allowed")
 })
 
 test("ignores number greater than 1000",()=>{
-    expect(Add("2,1003")).toBe(2)
-    expect(Add("1000,2")).toBe(1002)
+    expect(calculator.Add("2,1003")).toBe(2)
+    expect(calculator.Add("1000,2")).toBe(1002)
 })
 
 
 test("supports delimiters of any length using //[***]\\n syntax",()=>{
-    expect(Add("//[***]\n1***2***3")).toBe(6)
+    expect(calculator.Add("//[***]\n1***2***3")).toBe(6)
 })
 
 
 test("supports multiple delimiters using //[delim1][delim2]\\n syntax", () => {
-         expect(Add("//[*][%]\n1*2%3")).toBe(6);
+         expect(calculator.Add("//[*][%]\n1*2%3")).toBe(6);
      });
+
+  test("counts how many time Add method was called",()=>{
+    const newCalc=new Calculator();
+    newCalc.Add("1,2")
+    newCalc.Add("3,4")
+    newCalc.Add("8,9")
+    expect(newCalc.getAddMethodCounterValue()).toBe(3)
+  })   
     
+   
